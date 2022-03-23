78cb932720d2650e6c55b435b8f545334ef6b7fe
Severus7 <joshuasapz@gmail.com>
2022-03-24 07:29:20 +0800

    first commit

---
+++ src/components/Balance.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Balance = () => {
+  return (
+    <div>
+      <h4>Balance</h4>
+      <h1>Php 150.00</h1>
+    </div>
+  );
+};
+
+export default Balance;
---
+++ src/components/IncomeExpense.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const IncomeExpense = () => {
+  return (
+    <div className="inc-exp container">
+      <div>
+        <h4>Income</h4>
+        <p>Php 700.00</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p>Php 700.00</p>
+      </div>
+    </div>
+  );
+};
+
+export default IncomeExpense;
